48713ae45c592df8f1cc46ff0839b6427befe975
Kalyani Tarke <kr.tarke@gmail.com>
2025-08-29 22:16:12 +0530

    added adapter ,data class and 2 xml for sent and receiving message.

---
+++ app/src/main/java/com/example/rviewwithmultipleviews/ChatAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.rviewwithmultipleviews
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatAdapter(val messages:List<ChatMessage>):RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    private companion object{
+        const val TYPE_SENT=1
+        const val TYPE_RECEIVED=2
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
--- app/src/main/java/com/example/rviewwithmultipleviews/ChatMessage.kt
+++ app/src/main/java/com/example/rviewwithmultipleviews/ChatMessage.kt
@@ -1,3 +1,3 @@
 package com.example.rviewwithmultipleviews
-
+//true=user sent, false=received
 data class ChatMessage (val message: String,val isSent:Boolean)
