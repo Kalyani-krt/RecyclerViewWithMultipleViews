974510d3ad5bcec92ac2d1cab1cd02978a10ce31
Kalyani Tarke <kr.tarke@gmail.com>
2025-08-30 09:08:46 +0530

    Created 2 viewholder for 2 views i.e sent viewholder and received viewholder. then binded the data to respective VH then created data to display then set the recyclerview and adapter.

--- app/src/main/java/com/example/rviewwithmultipleviews/ChatAdapter.kt
+++ app/src/main/java/com/example/rviewwithmultipleviews/ChatAdapter.kt
@@ -1,28 +1,56 @@
 package com.example.rviewwithmultipleviews
 
+import android.view.InflateException
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ChatAdapter(val messages:List<ChatMessage>):RecyclerView.Adapter<RecyclerView.ViewHolder>(){
     //created static like constants
     private companion object{
         const val TYPE_SENT=1
-        const val TYPE_RECEIVED=2
+        const val TYPE_RECEIVED=2           //constants will be used by getItemViewType() to identify which layout to inflate.
     }
 
+    //called by recyclerview each time to know which view type that position uses
     override fun getItemViewType(position: Int): Int {
-        return super.getItemViewType(position)
+        return if(messages[position].isSent) TYPE_SENT else TYPE_RECEIVED   //checks the isSent flag on the message to return the correct type
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val inflater=LayoutInflater.from(parent.context)
+        return if(viewType== TYPE_SENT){
+            val v= inflater.inflate(R.layout.row_msg_sent,parent,false)
+            SentVH(v)       //create the appropriate ViewHolder
+        }
+        else{
+            val v=inflater.inflate(R.layout.row_msg_received,parent,false)
+            ReceivedVH(v)  //create the appropriate ViewHolder
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() =messages.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val msg=messages[position]
+       when(holder){     //because we have 2 view types here
+           is SentVH -> holder.bind(msg)
+           is ReceivedVH ->holder.bind(msg)   //fun bind sets the Textview to the message content
+       }
     }
 
+    class SentVH(itemView: View) :RecyclerView.ViewHolder(itemView){  //view holder for sent messages
+        private val sentView:TextView =itemView.findViewById(R.id.tv_msg)
+        fun bind(item: ChatMessage) {
+            sentView.text = item.message
+        }
+    }
+
+    class ReceivedVH(itemView: View) :RecyclerView.ViewHolder(itemView){    //view holder for receive messages
+        var receivedView:TextView =itemView.findViewById(R.id.tv_msg)
+        fun bind(item: ChatMessage){
+            receivedView.text=item.message
+        }
+    }
 }
--- app/src/main/java/com/example/rviewwithmultipleviews/MainActivity.kt
+++ app/src/main/java/com/example/rviewwithmultipleviews/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +20,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val data = listOf(
+            ChatMessage("Hey!", true),
+            ChatMessage("Hello 😊", false),
+            ChatMessage("How’s your prep going?", false),
+            ChatMessage("Grinding RecyclerView today 💪", true),
+            ChatMessage("Nice! Next, multi-view types?", false),
+            ChatMessage("Already on it 😎", true)
+        )
+
+        val rv:RecyclerView=findViewById(R.id.rv_chat)
+        val lm =LinearLayoutManager(this).apply{
+            stackFromEnd=true       //start list at the bottom like chats
+        }
+        rv.layoutManager=lm
+
+        rv.adapter=ChatAdapter(data)   //create the adapter with the message list and set it on the recyclerview
     }
 }
